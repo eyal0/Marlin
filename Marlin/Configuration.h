af8a63bc05b08cf584f5ef5910588c82e6a98eb1
Eyal Soha <eyalsoha@gmail.com>
2018-04-15 09:27:35 +0300

    increase default z jerk for UBL

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -622,7 +622,7 @@
  */
 #define DEFAULT_XJERK                 10.0
 #define DEFAULT_YJERK                 10.0
-#define DEFAULT_ZJERK                  0.09
+#define DEFAULT_ZJERK                  0.11
 #define DEFAULT_EJERK                  5.0
 
 /**
